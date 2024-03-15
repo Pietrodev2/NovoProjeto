fce9a8318d4ce5bd41b03bcc287a8d1ff87f752d
Pietro Vianna <pietrodfsan@gmail.com>
2024-03-15 08:19:48 -0300

    Author: Pietro 15/03

--- aulas/aula5/index.test.js
+++ aulas/aula5/index.test.js
@@ -1,11 +1,18 @@
+// Importando as bibliotecas necessárias
 const supertest = require("supertest");
-
 const app = require("./index");
 
+// Criando uma nova instância de supertest, passando a app como parâmetro
 const request = supertest(app);
 
+// Suite de teste para a requisição "GET" no /produtos endpoint
 test("Deve retornar status 200 e um JSON no GET", async function () {
+  // Enviando a requisição "GET" para o endpoint /produtos
   const response = await request.get("/produtos");
+
+  // Verificando se o código de status da resposta é igual a 200
   expect(response.status).toBe(200);
+
+  // Verificando se o tipo de conteúdo da resposta é JSON
   expect(response.headers["content-type"]).toMatch(/json/);
 });
--- aulas/aula5/router.js
+++ aulas/aula5/router.js
@@ -1,9 +1,14 @@
+// Importando o módulo express
 const express = require("express");
 
+// Criando uma nova instância do router
 const router = express.Router();
 
-router.get("/produtos", function(req,res){
-   res.json([])
-})
+// Manipulando a solicitação GET para o caminho "/produtos"
+router.get("/produtos", function(req, res) {
+    // Resposta enviada como um objeto JSON contendo um array vazio
+    res.json([]);
+});
 
+// Exportando o router para ser usado em outros módulos
 module.exports = router;
