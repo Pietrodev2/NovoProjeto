595f1a5e99621089160ee09318c1bb959f71d670
Pietro Vianna <pietrodfsan@gmail.com>
2024-05-17 10:45:07 -0300

    "Updated controller_produtos.js, router_produtos.js, swagger.yaml, and router_produtos.test.js files with new functionality and tests."

--- aulas/aula10/controllers/controller_produtos.js
+++ aulas/aula10/controllers/controller_produtos.js
@@ -1,31 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Produto = require('../models/model_produtos');
+const Produto = require("../models/model_produtos");
 
 async function validar(req, res, next) {
-    try {
-        const produto = new Produto(req.body);
-        await produto.validate();
-        next();
-    } catch(error) {
+  try {
+    const produto = new Produto(req.body);
+    await produto.validate();
+    next();
+  } catch (error) {
     res.status(422).json({});
-    }
-};
+  }
+}
 
 async function criar(req, res) {
-    const produto = await Produto.create(req.body);
-    res.status(201).json(produto);
-};
+  const produto = await Produto.create(req.body);
+  res.status(201).json(produto);
+}
 
 async function listar(req, res) {
-    const produtos = await Produto.find({});
-    res.json(produtos);
+  const produtos = await Produto.find({});
+  res.json(produtos);
+}
+
+async function buscar(req, res, next) {
+  try {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    const produto = await Produto.findOne({ _id: id });
+    if (produto) {
+      next();
+    } else {
+      res.status(404).json({ msg: "Produto não encontrado." });
+    }
+  } catch (error) {
+    res.status(400).json({ msg: "ID inválido." });
+  }
 }
 
 async function obter(req, res) {
-    const id = new mongoose.Types.ObjectId(req.params.id);
-    const produto = await Produto.findOne({ _id: id });
-    res.json(produto);
+  const id = new mongoose.Types.ObjectId(req.params.id);
+  const produto = await Produto.findOne({ _id: id });
+  res.json(produto);
 }
 
-module.exports = { criar, validar, listar, obter };
+async function atualizar(req, res){
+    res.json({})
+}
+
+module.exports = { criar, validar, listar, obter, buscar, atualizar };
--- aulas/aula10/routes/router_produtos.js
+++ aulas/aula10/routes/router_produtos.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
 const controllerProdutos = require("../controllers/controller_produtos");
-
 const router = express.Router();
 
 router.post("/", controllerProdutos.validar, controllerProdutos.criar);
 
 router.get("/", controllerProdutos.listar);
 
-router.get("/:id", controllerProdutos.obter);
+router.get("/:id", controllerProdutos.buscar, controllerProdutos.obter);
 
+router.put("/:id", controllerProdutos.atualizar);
 
 module.exports = router;
