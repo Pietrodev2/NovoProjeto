cca590af7c8552847d25ad196b700764c2fdaf12
Pietro Vianna <pietrodfsan@gmail.com>
2024-05-24 10:54:01 -0300

    "Updated package-lock.json, package.json, and various JavaScript files in aulas/aula10 and aulas/aula11 directories."

--- aulas/aula11/app.js
+++ aulas/aula11/app.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 var express = require('express');
 //var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URL)
 //var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- aulas/aula11/routes/users.js
+++ aulas/aula11/routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
+
+const controller = require("../controllers/users");
+
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/", controller.criar);
+
+router.post("/login", controller.entrar);
 
 module.exports = router;
