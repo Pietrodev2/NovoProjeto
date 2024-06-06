93ddf87202008048c8269ff3defe0a3224831196
Pietro Vianna <pietrodfsan@gmail.com>
2024-06-06 10:07:43 -0300

    aula 10 e 11 finalizada

--- aulas/aula11/controllers/users.js
+++ aulas/aula11/controllers/users.js
@@ -1,8 +1,10 @@
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
+
 const Usuario = require("../models/users");
 
 function cifrarSenha(senha, salt) {
+  console.log(senha, salt);
   const hash = crypto.createHmac("sha256", salt);
   hash.update(senha);
   return hash.digest("hex");
@@ -11,7 +13,12 @@ function cifrarSenha(senha, salt) {
 async function criar(req, res) {
   const salt = crypto.randomBytes(16).toString("hex");
   const senhaCifrada = cifrarSenha(req.body.senha, salt);
-  const novoUsuario = { email: req.body.email, senha: senhaCifrada, salt };
+  const novoUsuario = {
+    email: req.body.email,
+    senha: senhaCifrada,
+    salt,
+  };
+
   await Usuario.create(novoUsuario);
   res.status(201).json(novoUsuario);
 }
@@ -22,16 +29,34 @@ async function entrar(req, res) {
     const senhaCifrada = cifrarSenha(req.body.senha, usuario.salt);
     if (senhaCifrada === usuario.senha) {
       res.json({
-        token: jwt.sign({ email: usuario.email }, "12345678", {
-          expiresIn: "1m",
+        token: jwt.sign({ email: usuario.email }, process.env.SEGREDO, {
+          expiresIn: "3m",
         }),
       });
     } else {
-      res.status(401).json({ mensagem: "acesso negado" });
+      res.status(401).json({ msg: "acesso negado" });
     }
   } else {
-    res.status(400).json({ mensagem: "credenciais invalidas" });
+    res.status(400).json({ msg: "credenciais invalidas" });
   }
 }
 
-module.exports = { criar, entrar };
+function renovar(req, res) {
+  const token = req.headers["authorization"];
+  if (token) {
+    try {
+      const payload = jwt.verify(token, process.env.SEGREDO);
+      res.json({
+        token: jwt.sign({ email: payload.email }, process.env.SEGREDO, {
+          expiresIn: "3m",
+        }),
+      });
+    } catch (err) {
+      res.status(401).json({ msg: "token invalido" });
+    }
+  } else {
+    res.status(400).json({ msg: "Token não encontrado" });
+  }
+}
+
+module.exports = { criar, entrar, renovar };
